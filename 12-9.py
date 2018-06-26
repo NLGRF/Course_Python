796a3af02c876a00d3c40f0a8a5032f9f1511044
Non <non.devtech@gmail.com>
2018-06-27 00:43:15 +0700

    Edit 12-9

--- 12-9.py
+++ 12-9.py
@@ -2,7 +2,7 @@ import pymysql
 db = pymysql.connect( "localhost", "root",  "",  "order_db",
                       use_unicode=True, charset="utf8")       
 cursor = db.cursor()
-sql = "SELECT name, tel FROM custdetail WHERE custID 31 TO 35"
+sql = '''INSERT INTO custdetail(name, address, tel)
 VALUES ("โครงการบัณฑิตศึกษา", "18/18 ถ.บางนา-ตราด กม.18 บางพลี สมุทรปราการ","0-2337-0361")'''
 try:
     cursor.execute(sql)
